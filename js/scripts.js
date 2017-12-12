42c6ef16f80f203292ff4864b204c94eb270fea9
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-12 12:52:35 +0530

    Adding event listener to search button

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,22 @@ mapJSRef.onload = function () {
 document.getElementsByTagName("head")[0].appendChild(mapJSRef);
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-  	center: {lat: 18.5204, lng: 73.8567},
-  	zoom: 8
-  });
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 18.5204, lng: 73.8567},
+		zoom: 8
+	});
 }
+
+window.addEventListener('load', function() {
+	var btnSearchPlace = document.getElementById("btnSearchPlace");
+	var txtSearchPlace = document.getElementById("txtSearchPlace");
+	console.log('btnSearchPlace=>', btnSearchPlace);
+
+	btnSearchPlace.addEventListener("click", function(){
+		console.log('txtSearchPlace=>', txtSearchPlace);
+		console.log('txtSearchPlace=>', txtSearchPlace.value);
+		var replaced = txtSearchPlace.value.split(' ').join('+');
+		console.log('txtSearchPlace=>', replaced);
+	});
+
+});
