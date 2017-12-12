147208519808f0cc74f3ce7a4b53bbc80146cc9e
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-12 12:46:32 +0530

    Loading the maps & styled it

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
 console.log('script loaded');
 console.log('mapsKey=>', mapsKey);
+
+var mapJSRef = document.createElement('script');
+mapJSRef.setAttribute("type", "text/javascript");
+mapJSRef.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+ mapsKey.key +'&callback=initMap');
+mapJSRef.onload = function () {
+	console.log('mapJSRef loaded');
+	initMap();
+};
+
+document.getElementsByTagName("head")[0].appendChild(mapJSRef);
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+  	center: {lat: 18.5204, lng: 73.8567},
+  	zoom: 8
+  });
+}
